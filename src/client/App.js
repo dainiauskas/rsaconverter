2e34c0aaba80d05d14dfbfd8bec4ddd1ae8ae595
Dainius Matusevicius <dainiauskas@gmail.com>
2018-12-24 12:55:19 +0200

    RSA converter

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
+var FileInput = require('react-simple-file-input');
 import './app.css';
-import ReactImage from './react.png';
 
 export default class App extends Component {
-  state = { username: null };
+  state = {
+    username: null ,
+    value: '',
+    privateKey: '',
+    fileInput: null
+  };
 
-  componentDidMount() {
-    fetch('/api/getUsername')
+  updateIDRSAValue = (event) => {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.fileInput);
+
+    fetch('/api/convertRSA', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({ idRSA: this.state.value })
+    })
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      .then(jsonData => this.setState({ privateKey: jsonData.key }))
   }
 
   render() {
     const { username } = this.state;
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+      <div className="row">
+        <h1>{`RSA konverteris`}</h1>
+        <div className="column">
+          <p>ID_RSA content</p>
+          <form onSubmit={this.handleSubmit}>
+            <textarea rows="30" cols="80" value={this.state.value} onChange={this.updateIDRSAValue} />
+            <br/>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="column">
+          <p>PRIVATE KEY content</p>
+          <textarea rows="30" cols="80" value={this.state.privateKey} readOnly="readonly"></textarea>
+        </div>
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const os = require('os');
+const exec = require("child_process").exec
+const tmp = require('tmp');
+const fs = require('fs');
 
 const app = express();
 
+app.use(require("body-parser").json());
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.post('/api/convertRSA', (req, res) => {
+  var rsa = '-----BEGIN RSA PRIVATE KEY-----\n' + req.body.idRSA + '\n-----END RSA PRIVATE KEY-----\n'
+  var passfile = tmp.fileSync()
+
+  fs.writeFileSync(passfile.name, rsa)
+
+  exec(`openssl rsa -in ${passfile.name}`, (error, stdout, stderr) => {
+    fs.unlinkSync(passfile.name);
+
+    console.log('KEY generated');
+    return res.send({ key: stdout, error: stderr })
+  })
+
+});
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
